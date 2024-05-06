f3085b72b461f7f741f75ce525ad7da07baf1ea6
Wahyu Pranata <wahyupranata125@gmail.com>
2024-05-06 22:54:08 +0800

    Add cors

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,7 @@ const corsOptions: CorsOptions = {
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors(corsOptions));
 app.use(consoleLogger);
 
 app.get("/", (req: Request, res: Response) => {
