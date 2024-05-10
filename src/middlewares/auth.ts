f0e4915a1a7898c559d2db5a9312b6de15a1c477
Wahyu Pranata <wahyupranata125@gmail.com>
2024-05-10 12:28:04 +0800

    Turn off middleware temp

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -6,23 +6,5 @@ export const isAuthenticated = (
   res: Response,
   next: NextFunction,
 ) => {
-  try {
-    const token = req.query.token;
-    if (!token) {
-      res.status(403).json({ message: "Sorry, you are not authorized..." });
-      return;
-    }
-    console.log(token);
-    verify(token as string, process.env.ACCESS_TOKEN_SECRET as string);
-    next();
-  } catch (err) {
-    if (err instanceof TokenExpiredError) {
-      res.status(403).json({ message: "expired token" });
-    } else if (err instanceof JsonWebTokenError) {
-      res.status(403).json({ message: "malformed token (your token wrong)" });
-    } else {
-      console.log(err);
-      res.status(500).json({ message: "something went wrong" });
-    }
-  }
+  next();
 };
