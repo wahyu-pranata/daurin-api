0b030b9e153465c8e7a847801ab3058e1e98b3a7
Wahyu Pranata <wahyupranata125@gmail.com>
2024-05-10 09:40:48 +0800

    Update user address

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,14 +1,19 @@
-import { Request, Response } from 'express';
-import * as bcrypt from 'bcrypt';
-import { PrismaClient } from '@prisma/client';
+import { Request, Response } from "express";
+import * as bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
 
-import { RegisterUser, LoginUser, CreateUser, userRegisterValidation } from '../entity/user.entity';
-import  { generateAccessToken, decodeToken } from '../helper/jwt';
-import { ZodError } from 'zod';
+import {
+  RegisterUser,
+  LoginUser,
+  CreateUser,
+  userRegisterValidation,
+} from "../entity/user.entity";
+import { generateAccessToken, decodeToken } from "../helper/jwt";
+import { ZodError } from "zod";
 
 const prisma = new PrismaClient();
 
-export  const userRegister = async (req: Request, res: Response) => {
+export const userRegister = async (req: Request, res: Response) => {
   try {
     const data: RegisterUser = req.body;
     const parsedData = userRegisterValidation.parse(data);
@@ -18,25 +23,24 @@ export  const userRegister = async (req: Request, res: Response) => {
 
     const userData: CreateUser = {
       ...parsedData,
-      role: 'Customer',
-      address: '-',
-      agencyId: null
+      role: "Customer",
+      address: "-",
+      agencyId: null,
     };
 
     const user = await prisma.user.create({
-      data: userData
+      data: userData,
     });
 
-    if(user) {
-      return res.status(201).json({ message: 'User successfully created' });
+    if (user) {
+      return res.status(201).json({ message: "User successfully created" });
     }
-    
-  } catch(err: any) {
-    if(err.name == 'ZodError') {
+  } catch (err: any) {
+    if (err.name == "ZodError") {
       res.status(400).json(err.issues);
     }
   }
-}
+};
 
 export const userLogin = async (req: Request, res: Response) => {
   try {
@@ -44,39 +48,48 @@ export const userLogin = async (req: Request, res: Response) => {
 
     const user = await prisma.user.findFirst({
       where: {
-        email: data.email
-      }
-    })
+        email: data.email,
+      },
+    });
 
-    if(!user) {
-      return res.status(404).json({ message: 'The credentials you provided doesn\'t match our records. Please try again' });
+    if (!user) {
+      return res.status(404).json({
+        message:
+          "The credentials you provided doesn't match our records. Please try again",
+      });
     }
-    
+
     const isPasswordMatch = await bcrypt.compare(data.password, user.password);
-    
-    if(!isPasswordMatch) {
-      return res.status(404).json({ message: 'The credentials you provided doesn\'t match our records. Please try again' });
+
+    if (!isPasswordMatch) {
+      return res.status(404).json({
+        message:
+          "The credentials you provided doesn't match our records. Please try again",
+      });
     }
 
     const tokens = generateAccessToken(user);
     res
       .status(200)
-      .cookie('token', tokens.accessToken, { httpOnly: true })
-      .json({ message: "Login success", accessToken: tokens.accessToken, refreshToken: tokens.refreshToken });
-
+      .cookie("token", tokens.accessToken, { httpOnly: true })
+      .json({
+        message: "Login success",
+        accessToken: tokens.accessToken,
+        refreshToken: tokens.refreshToken,
+      });
   } catch (err: any) {
-    if(err instanceof ZodError) {
+    if (err instanceof ZodError) {
       console.log(err.message);
-    } else if(err instanceof Error) {
+    } else if (err instanceof Error) {
       console.log(err.message);
     }
     res.status(500).json({ message: "Something went wrong in server..." });
   }
-}
+};
 
 export const getUser = (req: Request, res: Response) => {
   const { token } = req.body;
 
   const userData = decodeToken(token);
   return res.status(200).json({ user: userData });
-} 
+};
--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -1,6 +1,6 @@
-import { z } from 'zod';
-import { isEmail } from 'validator';
-import { UserRole } from '@prisma/client';
+import { z } from "zod";
+import { isEmail } from "validator";
+import { UserRole } from "@prisma/client";
 
 export type CreateUser = {
   name: string;
@@ -8,28 +8,38 @@ export type CreateUser = {
   password: string;
   role: UserRole;
   address: string;
+  city: string;
+  province: string;
   agencyId: number | null;
-}
+};
 
 export type RegisterUser = {
   name: string;
   email: string;
   password: string;
-}
+  city: string;
+  province: string;
+  address: string;
+};
 
 export type LoginUser = {
   email: string;
   password: string;
-}
+};
 
 export const userRegisterValidation = z.object({
   name: z.string(),
-  email: z.string().refine(isEmail, { message: 'Pleae enter valid email address' }),
-  password: z.string()
+  email: z
+    .string()
+    .refine(isEmail, { message: "Pleae enter valid email address" }),
+  password: z.string(),
+  city: z.string(),
+  province: z.string(),
 });
 
-
 export const userLoginValidation = z.object({
-  email: z.string().refine(isEmail, { message: 'Please enter valid email address' }),
-  password: z.string()
+  email: z
+    .string()
+    .refine(isEmail, { message: "Please enter valid email address" }),
+  password: z.string(),
 });
